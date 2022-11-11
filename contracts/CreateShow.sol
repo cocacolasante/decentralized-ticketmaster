5588b7a4b92d63d58f227a43fb1e15f344c61ac6
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-11-10 20:07:05 -0500

    create ticket show contract

---
+++ contracts/CreateShow.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "hardhat/console.sol";
+
+import "./TicketsERC1155.sol";
+
+contract CreateShow{
+    address payable public admin;
+
+    uint public showCount;
+
+    mapping(uint=>Show) public allShows;
+    mapping(uint => DegenTickets) public allContracts;
+
+    struct Show{
+        address band;
+        address venue;
+        address ticketContract;
+    }
+
+    constructor(){
+        admin = payable(msg.sender);
+    }
+
+    function createNewShowTickets(
+            string memory tokenUri,
+            uint ticketPrice,
+            uint bandPercentage,
+            address bandAddress,
+            address venueAddress,
+            uint maxTickets 
+        ) public {
+
+            showCount++;
+
+
+            DegenTickets newShowTickets = new DegenTickets(tokenUri, ticketPrice, bandPercentage, bandAddress, venueAddress, maxTickets);
+            allContracts[showCount] = newShowTickets;
+
+            Show memory newShow = Show(bandAddress, venueAddress, address(newShowTickets));
+
+            allShows[showCount] = newShow;
+
+        }
+    
+}
